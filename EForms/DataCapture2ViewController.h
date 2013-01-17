c163c52bcdfd824d5ae74bce4e3404b8f6c2c52f
vmmobidev <vmmobidev@gmail.com>
2013-01-17 16:17:07 +0530

    first commit

---
+++ EForms/DataCapture2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  DataCapture2ViewController.h
+//  EForms
+//
+//  Created by Rahul kumar on 1/15/13.
+//  Copyright (c) 2013 vmoksha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataCapture2ViewController : UIViewController
+
+@end
---
+++ EForms/DataCapture2ViewController.m
@@ -0,0 +1,38 @@
+//
+//  DataCapture2ViewController.m
+//  EForms
+//
+//  Created by Rahul kumar on 1/15/13.
+//  Copyright (c) 2013 vmoksha. All rights reserved.
+//
+
+#import "DataCapture2ViewController.h"
+
+@interface DataCapture2ViewController ()
+
+@end
+
+@implementation DataCapture2ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
