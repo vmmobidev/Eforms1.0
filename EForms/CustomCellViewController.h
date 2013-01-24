ac88ded8e357abeb4f682be41d56e31092ba3c6f
vmmobidev <vmmobidev@gmail.com>
2013-01-24 15:50:56 +0530

    changes

---
+++ EForms/CustomCellViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomCellViewController.h
+//  EForms
+//
+//  Created by Rahul kumar on 1/23/13.
+//  Copyright (c) 2013 vmoksha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCellViewController : UIViewController
+
+@end
---
+++ EForms/CustomCellViewController.m
@@ -0,0 +1,38 @@
+//
+//  CustomCellViewController.m
+//  EForms
+//
+//  Created by Rahul kumar on 1/23/13.
+//  Copyright (c) 2013 vmoksha. All rights reserved.
+//
+
+#import "CustomCellViewController.h"
+
+@interface CustomCellViewController ()
+
+@end
+
+@implementation CustomCellViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
